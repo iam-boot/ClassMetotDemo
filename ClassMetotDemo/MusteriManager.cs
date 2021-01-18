213728f6035d5cbff1e7b287dfe635595a20436e
byR00T <bydemirel.root@hotmail.com>
2021-01-18 06:41:33 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public static List<Musteri> musteriListe = new List<Musteri>();
+        public void Add(Musteri musteri)
+        {
+
+            Console.Write("Müşteri Adı Soyadı :");
+            musteri.Name = Console.ReadLine();
+            Console.Write("Müşteri Cinsiyeti : Erkek(0) Kadın(1)");
+            int selection2;
+            var UserInput2 = Console.ReadKey();
+            Console.WriteLine();
+            if (char.IsDigit(UserInput2.KeyChar))
+            {
+                selection2 = int.Parse(UserInput2.KeyChar.ToString());
+                if (selection2 > -1 && selection2 < 2)
+                {
+                    switch (selection2)
+                    {
+                        case 0:
+                            musteri.Gender = true;
+                            break;
+                        case 1:
+                            musteri.Gender = false;
+                            break;
+
+                    }
+                }
+                else
+                    Console.WriteLine("Yanlış seçim yaptınız.");
+            }
+            else
+                Console.WriteLine("Yanlış seçim yaptınız.");
+            Console.WriteLine("Müşteri Giriş Bakiyesi (Küsuratı virgül kullanarak belirtin):");
+            try
+            {
+                musteri.Balance = double.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Değeri Yanlış Girdiniz.");
+            }
+
+            Console.WriteLine(musteri.Balance);
+            Program.musteriListe.Add(musteri);
+            Console.WriteLine("{0} adlı müşteri olüşturuldu.",musteri.Name);
+            Console.WriteLine("********************Müsteri Bilgileri******************");
+            Console.WriteLine("Müşteri ID: {0}\nMüşteri Cinsiyeti : {1}\nMüşteri Bakiye : {2}\n",musteri.Id,musteri.Gender,musteri.Balance);
+            
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void Liste(List<Musteri> musteriListe)
+        {
+            foreach (var musteri in musteriListe)
+            {
+                Console.WriteLine(musteri);
+            }
+        }
+
+ 
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        public static List<Musteri> musteriListe = new List<Musteri>();
+        static void Main(string[] args)
+        {
+
+            Musteri musteri = new Musteri();
+
+
+            Console.WriteLine("Test Bank");
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            while (true)
+            {
+                Console.WriteLine("-------------Müşteri İşlemleri-------------");
+                Console.WriteLine("1 - Müsteri Ekleme\n2 - Müşteri Silme\n3 - Müşteri Listeleme\nLütfen seçiminizi belirtiniz.");
+
+                int selection1;
+                var UserInput1 = Console.ReadKey();
+                Console.WriteLine();
+                if (char.IsDigit(UserInput1.KeyChar))
+                {
+                    selection1 = int.Parse(UserInput1.KeyChar.ToString());
+                    if (selection1 > 0 && selection1 < 4)
+                    {
+                        switch (selection1)
+                        {
+                            case 1:
+
+                                musteriManager.Add(musteri);
+
+
+                                break;
+                            case 2:
+
+                                break;
+                            case 3:
+                                musteriManager.Liste(musteriListe);
+                                break;
+                        }
+                    }
+                    else
+                        Console.WriteLine("Yanlış seçim yaptınız.");
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış seçim yaptınız.");
+                }
+
+                Console.WriteLine("Çıkmak için : 0");
+                int selection2;
+                var UserInput2 = Console.ReadKey();
+                Console.WriteLine();
+                if (char.IsDigit(UserInput2.KeyChar))
+                {
+                    selection2 = int.Parse(UserInput2.KeyChar.ToString());
+                    if (selection2 > -1 && selection2 < 2)
+                    {
+                        if(selection2 == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+
+
+
+
+        }
+
+    }
+
+}
